bed1e6482eece559056e1efb368644652cf3515c
Laxman Bommidi <bommidilaxman1017@gmail.com>
2025-03-13 00:45:23 +0530

    remove handler added

--- src/app/display-emp/display-emp.component.ts
+++ src/app/display-emp/display-emp.component.ts
@@ -1,5 +1,5 @@
 import { Component , Input} from '@angular/core';
-import { KENDO_GRID } from "@progress/kendo-angular-grid";
+import { KENDO_GRID, RemoveEvent } from "@progress/kendo-angular-grid";
 import { EmployeeModel } from '../model';
 @Component({
   selector: 'app-display-emp',
@@ -9,4 +9,13 @@ import { EmployeeModel } from '../model';
 })
 export class DisplayEmpComponent {
   @Input() empList : EmployeeModel[] = [];
+
+
+  public removeHandler(event: RemoveEvent): void {
+    const index = this.empList.findIndex(emp => emp.empid === event.dataItem.empid);
+    if (index !== -1) {
+      this.empList.splice(index, 1);
+      localStorage.setItem("EmpData", JSON.stringify(this.empList))
+    }
+  }
 }
